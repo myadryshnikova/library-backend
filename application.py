24ba7326502494d1ad582e659f67f13ec7c41203
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2023-12-27 15:29:30 +0500

    feat(user): add user seeding then application starts

--- application.py
+++ application.py
@@ -7,12 +7,16 @@ from datetime import timedelta
 
 from library_api.domain.data_access_layer.build_connection_string import build_connection_string
 from library_api.domain.data_access_layer.db import db, migrate
+from library_api.domain.user import User
 
 from library_api.modules.auth.auth_routes import auth_blueprint
+from library_api.modules.auth.commands.new_user_command import NewUserCommand
 from library_api.modules.books.books_routes import books_blueprint
 from library_api.modules.bookshelfs.bookshelfs_routes import bookshelfs_blueprint
 from library_api.modules.citations.citations_routes import citations_blueprint
 from library_api.modules.readings.reading_routes import readings_blueprint
+from library_api.modules.auth.services.auth_service import AuthService
+from library_api.modules.auth.queries.user_query import GetUser
 
 
 def create_app():
@@ -45,6 +49,19 @@ def create_app():
     with app.app_context():
         _upgrade()
 
+    username = "user"
+    password = "password"
+    user = GetUser().by_username(username)
+
+    if not user:
+        password_props = AuthService().get_hashed_password(password)
+        user_entity = User(
+            login=username,
+            hashed_password=password_props.hashed_password,
+            password_salt=password_props.salt
+        )
+
+        NewUserCommand().add_user(user_entity)
 
     return app
 
